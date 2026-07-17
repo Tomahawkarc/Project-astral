87166ce4a51b5047e65dc49f5d5526c45759973f
Tomahawkarc <dolsgokols@gmail.com>
2026-07-18 01:19:16 +0300

    Create constants.js

---
+++ project-astral-blueprint/scripts/core/constants.js
@@ -0,0 +1,16 @@
+"use strict";
+
+exports.mod = {
+    id: "project-astral",
+    contentPrefix: "project-astral-",
+    stateKey: "project-astral-campaign-state",
+    rulesTag: "project-astral-state",
+    schema: 1
+};
+
+exports.engine = {
+    minimum: "159.6",
+    targetBuild: 159,
+    targetRevision: 6,
+    sourceCommit: "167be4e4a545f62f700185e0934834906f74c1f1"
+};
